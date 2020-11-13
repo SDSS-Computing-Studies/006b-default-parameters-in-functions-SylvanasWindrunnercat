ca0ec0e565a4c084be89561ec0e97e83980f8697
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 13:19:51 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,21 +1,7 @@
 #!python3
-def factor(a):
-    output = []
-    i = 2
-    while a // i > 0:
-        if a % i == 0:
-            output.append(i)
-            a //= i
-        else: i += 1
-    return(output)
 
-print(factor(12))
+import assignment
 
-b = int(input("The number1 is:"))
-c = int(input("The number2 is:"))
-
-d = b * c
-
-x = factor(d)
+x = assignment.factorPair(24,12)
 
 print(x)
