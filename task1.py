af4bb265728d2de0a704aa7cb3330236c5f75a9d
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 13:18:32 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,20 +2,6 @@
 
 import assignment
 
-
-def tempConversion(a,b):
-    if b == "F":
-        a = int(a)
-        c = 5 * a / 9
-        return c
-    else:
-        a = int(a)
-        d = 1.8 * a + 32
-        return d
-
-c = input("The number is:")
-d = input("It is celsius or fahrenheit:")
-
-x = tempConversion(c,d)
+x = assignment.tempConversion(72,"F")
 
 print(x)
